01f5862b9639ffe090dc78f1bf52754bce3bfe63
Rhyme-Penamora <95707435+Rhyme-Penamora@users.noreply.github.com>
2026-05-15 23:47:12 +0800

    Create __init__.py

---
+++ editor/__init__.py
@@ -0,0 +1,6 @@
+# =============================================================================
+# File: sandbox_game/editor/__init__.py
+# =============================================================================
+"""
+Editor systems: code editor, tile editor, entity editor, sprite editor, file editor, inventory editor.
+"""
